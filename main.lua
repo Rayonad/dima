be777cdbb6df8ff95a5b3bbb8ed489384940c871
Rayonad <57120397+Rayonad@users.noreply.github.com>
2025-04-21 23:50:52 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,14 @@
+-- Загружаем модули
 local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Rayonad/dima/refs/heads/master/gui.lua"))()
 local NapeModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Rayonad/dima/refs/heads/master/nape.lua"))()
 
-GUI:AddModule("Test", function(enabled)
-    if enabled then
-        TestModule.start()
-    else
-        TestModule.stop()
+-- Вручную добавляем переключатель для Nape (альтернатива AddModule)
+GUI.toggles["Nape"] = {
+    enabled = false,
+    callback = function(enabled)
+        NapeModule.Toggle(enabled)
     end
-end)&#8203;:contentReference[oaicite:7]{index=7}
+}
+
+-- Если есть другие модули, добавляем их так же
+-- GUI.toggles["Aim"] = { ... }
