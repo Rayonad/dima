8c257249d3de51f58e128434b803ae1f0a150f3f
Rayonad <57120397+Rayonad@users.noreply.github.com>
2025-04-25 18:49:40 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,4 +1,13 @@
-while true do
-    print("Hello world")  -- Вывод в консоль
-    wait(2)  -- Пауза 2 секунды
+local running = false
+
+function start()
+    running = true
+    while running do
+        print("Hello world")  -- Вывод в консоль
+        wait(2)  -- Пауза 2 секунды
+    end
+end
+
+function stop()
+    running = false
 end
